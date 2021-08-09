b4e316d6d11609e1e5e3656fe6a1fc5c8c8a0ec7
jspeers101 <johnhtc101@gmail.com>
2021-08-09 07:17:14 -0600

    hi

--- FundamentalAnalysis/CLutchChapStonkAnalysis.py
+++ FundamentalAnalysis/CLutchChapStonkAnalysis.py
@@ -5,13 +5,30 @@ import yfinance as yf
 
 
 def stockratios(ticker):
-    # ratioDf = pd.DataFrame(columns=['peg', 'trailingpe', 'ebitda'])
     ticker = yf.Ticker(ticker)
-    ratiodf = pd.DataFrame([ticker.info['pegRatio'], ticker.info['trailingPE'], ticker.info['ebitda']])
+    ratiodf = pd.DataFrame([ticker.info['shortName'],
+                            ticker.info['currentPrice'],
+                            ticker.info['marketCap'] / 1e9,
+                            ticker.info['totalRevenue'] / 1e9,
+                            ticker.info['shortPercentOfFloat']*100,
+                            ticker.info['pegRatio'],
+                            # ticker.info['trailingPE'],
+                            ticker.info['trailingEps'],
+                            ticker.info['forwardEps'],
+                            ticker.info['priceToSalesTrailing12Months'],
+                            ticker.info['enterpriseToEbitda'],
+                            ticker.info['totalCashPerShare'],
+                            ticker.info['revenueGrowth']*100])
     ratiodf = ratiodf.T
-    ratiodf.columns = ['PEG', 'PE (TTM)', 'EBITDA']
+    ratiodf.columns = ['Company Name', 'Price', 'Market Cap (b)', 'Revenue (b)', 'Short %',
+                       'PEG', 'EPS (TTM)', 'EPS (NTM)', 'P/S (TTM)',
+                       'EV/EBITDA', 'Cash Per Share', 'Revenue Growth (%)']
     return ratiodf
 
 
-aapl = stockratios('AAPL')
-show(aapl)
+stonks = pd.concat([stockratios('AAPL'),
+                    stockratios('MSFT'),
+                    stockratios('PLTR'),
+                    stockratios('AMD'),
+                    stockratios('PSFE')])
+show(stonks)
